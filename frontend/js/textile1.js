24ddbc9ae95671c543345add1e83fba4a64e5df2
DeepikaSaravanan5 <deepika008@e.ntu.edu.sg>
2024-11-12 20:26:09 +0800

    Update textile1.js

--- frontend/js/textile1.js
+++ frontend/js/textile1.js
@@ -1,6 +1,10 @@
 function storeTextileQuantity() {
     const quantity = document.getElementById('textileQuantity').value;
-
+  
+    if (Number.isInteger(Number(quantity))== false|| quantity <= 0)
+        { alert("Quantity must be a whole number greater than 0"); 
+            return false; 
+        }
 
     if (quantity >= 10) {
      
